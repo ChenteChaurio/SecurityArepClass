8ecac48a7491aafb2e3ef933c5f6d70226d13f64
unknown <vicente.garzon-r@labinfo.is.escuelaing.edu.co>
2026-03-17 15:18:05 -0500

    Todo el trabajo en clase

---
+++ springsecure/src/main/java/com/mycompany/springsecure/Secureweb.java
@@ -0,0 +1,15 @@
+package com.mycompany.springsecure;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secureweb {
+    public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(Secureweb.class);
+    app.setDefaultProperties(Collections.singletonMap("server.port", "5000"));
+    app.run(args);
+    }
+} 
---
+++ springsecure/src/main/java/com/mycompany/springsecure/Springsecure.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.springsecure;
+
+/**
+ *
+ * @author vicente.garzon-r
+ */
+public class Springsecure {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
